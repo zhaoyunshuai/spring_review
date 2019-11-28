6e342bf2ad55fc81061ce5f0b31477540b927711
zhaoshuai <13022199358@163.com>
2019-11-28 22:24:48 +0800

    AspectJ的，注解配置方式

--- spring_aspectJ_annotation/src/main/java/cn/zs/springAspectJAnnotation/aspect/MyAspect.java
+++ spring_aspectJ_annotation/src/main/java/cn/zs/springAspectJAnnotation/aspect/MyAspect.java
@@ -1,9 +1,11 @@
 package cn.zs.springAspectJAnnotation.aspect;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import sun.awt.SunHints;
 
 
 @Aspect
@@ -12,5 +14,28 @@ public class MyAspect {
     @Before("execution(* cn.zs.springAspectJAnnotation.service.impl.CustomerServiceImpl.*(..))||execution(* cn.zs.springAspectJAnnotation.service.impl.ProductService.*(..))")
     public void before(JoinPoint joinPoint) {
         System.out.println("我是前置通知，我会在方法执行之前去执行.....");
+    }
+
+    @AfterReturning(value = "bean(*Service)", returning = "value")
+    public void afterReturning(JoinPoint joinPoint, Object value) {
+        System.out.println("----我是后置通知----");
+
+    }
+
+    @Around("bean(*Service)")
+    public Object Around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("我是环绕通知，环绕在方法之前QQQQQ");
+        Object proceed = proceedingJoinPoint.proceed();
+        System.out.println("我是环绕通知，环绕在方法之后HHHHH");
+        return proceed;
+    }
+    @After("bean(*Service)")
+    public void after(JoinPoint joinPoint){
+        System.out.println("我是最终通知0101010");
+    }
+@AfterThrowing(value = "bean(*Service)",throwing = "e")
+    public void afterThrowing(JoinPoint joinPoint,Exception e){
+        System.out.println("异常通知，也叫做抛出通知"+ e.getMessage());
+
     }
 }
--- spring_aspectJ_annotation/src/main/java/cn/zs/springAspectJAnnotation/service/impl/ProductService.java
+++ spring_aspectJ_annotation/src/main/java/cn/zs/springAspectJAnnotation/service/impl/ProductService.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service("productService")
 public class ProductService {
-    public  void save(){
+    public void save() {
         System.out.println("没有实现接口：信息被保存了........");
     }
-    public int find(){
+
+    public int find() {
         System.out.println("没有接口实现：信息被找到了......");
-        return  10086;
+        int a =1/0;
+        return 10086;
     }
 }
