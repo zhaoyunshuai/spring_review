3112c6a9016071ffe7fc4b6d0c38e7322e54dbb4
zhaoshuai <13022199358@163.com>
2019-12-01 22:23:23 +0800

    spring-Transaction 修改配置文件中的命名空间。

--- spring_transaction_anotation/src/main/java/springTransaction/service/impl/AccountServiceImpl.java
+++ spring_transaction_anotation/src/main/java/springTransaction/service/impl/AccountServiceImpl.java
@@ -18,7 +18,7 @@ public class AccountServiceImpl implements IAccountService {
     public void transfer(String outUsername, String inUsername, Double money) {
         accountDao.out(outUsername, money);
         //此处打开，可以直接验证事务是否生效。
-       // int i = 1 / 0;
+        //int i = 1 / 0;
         accountDao.in(inUsername, money);
     }
 }
